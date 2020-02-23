196b178011611ee0c68aeae6a1bb759ef874af8a
alonlong <alonlong@163.com>
2020-02-23 11:17:21 +0800

    fix the migration sql

--- migrations/00005_create_posts_tags_table.sql
+++ migrations/00005_create_posts_tags_table.sql
@@ -1,11 +1,9 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 CREATE TABLE `posts_tags` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int unsigned NOT NULL DEFAULT 0,
   `tag_id` int unsigned NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (`id`),
   CONSTRAINT `post_tags` UNIQUE (post_id, tag_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
