7ba570093fb43bbbea55959d765275dc9c52a7c0
alonlong <alonlong@163.com>
2020-03-06 18:04:15 +0800

    fix the handlers logic for the routes design

--- httpd/handlers/getPosts.go
+++ httpd/handlers/getPosts.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -9,34 +11,124 @@ import (
 	"github.com/jjcm/soci-backend/models"
 )
 
-// GetPosts - get all the posts in the system
+// GetPosts - get the posts from database with different url parameters
 func GetPosts(w http.ResponseWriter, r *http.Request) {
-	// check duration of 24 hours vs. last login
-	user := models.User{}
-	user.FindByID(r.Context().Value("user_id").(int))
+	// parse the url parameters
+	r.ParseForm()
 
-	lastLogin := user.LastLogin
-	twenty24HoursAgo := time.Now().AddDate(0, 0, -1)
-	cutoff := lastLogin
-	if lastLogin.After(twenty24HoursAgo) {
-		cutoff = twenty24HoursAgo
+	// get the user id from context
+	userID := r.Context().Value("user_id").(int)
+	// query the user by user id
+	user := &models.User{}
+	if err := user.FindByID(userID); err != nil {
+		sendSystemError(w, fmt.Errorf("Query user: %v", err))
+		return
 	}
 
-	// check for offset
-	r.ParseForm()
-	formOffset := r.FormValue("offset")
 	offset := 0
-	if strings.TrimSpace(formOffset) != "" {
+	// check for offset
+	formOffset := strings.TrimSpace(r.FormValue("offset"))
+	if formOffset != "" {
 		var err error
 		offset, err = strconv.Atoi(formOffset)
 		if err != nil {
-			sendSystemError(w, err)
+			sendSystemError(w, fmt.Errorf("String to int: %v", err))
 			return
 		}
 	}
+
+	// ?time=all|day|week|month|year
+	// Only returns posts that were created within a specific time period.
+	// - all DEFAULT
+	//   No time constraints.
+	// - day
+	//   Only returns posts created in the last day.
+	// - week
+	//   Only returns posts created in the last week.
+	// - month
+	//   Only returns posts created in the last month.
+	// - year
+	//   Only returns posts created in the last year.
+	//
+	// parse the time value
+	var cutoff time.Time
+	switch formTime := strings.TrimSpace(r.FormValue("time")); formTime {
+	case "day":
+		cutoff = time.Now().AddDate(0, 0, -1)
+	case "week":
+		cutoff = time.Now().AddDate(0, 0, -7)
+	case "month":
+		cutoff = time.Now().AddDate(0, -1, 0)
+	case "year":
+		cutoff = time.Now().AddDate(-1, 0, 0)
+	case "all":
+		cutoff = time.Now().AddDate(-50, 0, 0)
+	case "": // if it's empty, default cutoff
+		// check duration of 24 hours vs last login
+		cutoff = user.LastLogin
+		oneDayAgo := time.Now().AddDate(0, 0, -1)
+		if user.LastLogin.After(oneDayAgo) {
+			cutoff = oneDayAgo
+		}
+	default:
+		sendSystemError(w, fmt.Errorf("Invalid field 'time': %v", formTime))
+		return
+	}
+
+	// ?sort=popular|top|new
+	// Returns posts sorted by a particular algorithm.
+	// - popular DEFAULT
+	//   Posts are sorted by score, within a time span between now and the requesting user's last login, or 24 hours, whichever is longer.
+	// - new
+	//   Posts are sorted by date. Newest first.
+	// - top
+	//   Posts are sorted by score. Highest first.
+	sort := strings.TrimSpace(r.FormValue("sort"))
+	if sort != "" {
+		var posts []models.Post
+
+		var err error
+		switch sort {
+		case "popular":
+			// parse the tag name
+			formTag := strings.TrimSpace(r.FormValue("tag"))
+			if formTag == "" {
+				sendSystemError(w, errors.New("Tag is empty"))
+				return
+			}
+			// query the posts by tag name
+			posts, err = models.GetPostsByPostTagScoreSince(formTag, cutoff)
+
+		case "new":
+			// query the posts by the offset
+			posts, err = models.GetLatestPosts(offset)
+
+		case "top":
+			// query the posts by the cutoff and offset
+			posts, err = models.GetPostsByScoreSince(cutoff, offset)
+
+		default:
+			sendSystemError(w, fmt.Errorf("Invalid field 'sort': %v", sort))
+			return
+		}
+		if err != nil {
+			sendSystemError(w, fmt.Errorf("Query posts by sort=%s: %v", sort, err))
+			return
+		}
+
+		// send the result back to the clients
+		output := map[string]interface{}{
+			"posts": posts,
+		}
+		SendResponse(w, output, 200)
+		return
+	}
+
+	// ?offset=NUMBER
+	// Offsets the responses by a set number.
 	posts, err := models.GetPostsByScoreSince(cutoff, offset)
 	if err != nil {
-		sendSystemError(w, err)
+		sendSystemError(w, fmt.Errorf("Query posts by offset: %v", err))
 		return
 	}
 
@@ -44,4 +136,5 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		"posts": posts,
 	}
 	SendResponse(w, output, 200)
+	return
 }
