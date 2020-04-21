8fa11429b81399f28cd3971495ddd81d2d469744
Jake Miller <j@jjcm.org>
2020-04-21 18:55:20 +1000

    added getVotes handler

---
+++ httpd/handlers/getVotes.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+
+	"soci-backend/models"
+)
+
+// GetVotes - gets a user's posttagvotes.
+func GetVotes(w http.ResponseWriter, r *http.Request) {
+	u := models.User{}
+	u.FindByID(r.Context().Value("user_id").(int))
+
+	votes, err := u.MyVotes()
+	if err != nil {
+		SendResponse(w, MakeError(err.Error()), 500)
+		return
+	}
+
+	output := map[string]interface{}{
+		"votes": votes,
+	}
+	SendResponse(w, output, 200)
+}
