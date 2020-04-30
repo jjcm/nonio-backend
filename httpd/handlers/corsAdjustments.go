7ce9ab804670dd7d2f44f10252f59a1168a12fc5
Jake Miller <j@jjcm.org>
2020-04-30 20:30:31 +1000

    made CORS more flexible

--- httpd/handlers/corsAdjustments.go
+++ httpd/handlers/corsAdjustments.go
@@ -4,6 +4,8 @@ import "net/http"
 
 // CorsAdjustments - additional CORS headers can be attached as necessary
 func CorsAdjustments(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
 }
