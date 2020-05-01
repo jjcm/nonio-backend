264f051a30e49a0d6b2b3d490de04cbfa3d539b2
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2020-05-01 16:37:22 -0700

    wtf

--- build.sh
+++ build.sh
@@ -10,8 +10,6 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ../dist/socid
 echo "done!"
 
 # maybe in the future there will be other things that we need to build...
-
-# maybe in the future there will be other things that we need to build...
 export APP_KEY=superdupersecretyayyayyayyyyyyyyyy
 export OAUTH_ID=0ed06b35279d956038d7
 export OAUTH_SECRET=2af443a467821d992837d2ae1ca6af175f413af5
