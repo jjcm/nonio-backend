1560c41af0bede7289a84d44b89a12c5ca42fa69
jjcm <j@jjcm.org>
2020-07-19 15:16:09 +1000

    testing votes in the route to see if those work

--- httpd/handlers/getSubscriptions.go
+++ httpd/handlers/getSubscriptions.go
@@ -11,7 +11,7 @@ func GetSubscriptions(w http.ResponseWriter, r *http.Request) {
 	u := models.User{}
 	u.FindByID(r.Context().Value("user_id").(int))
 
-	subscriptions, err := u.MySubscriptions()
+	subscriptions, err := u.MyVotes()
 	if err != nil {
 		SendResponse(w, MakeError(err.Error()), 500)
 		return
