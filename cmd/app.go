03a7b0e1f4eb7840504b32e29a9fa85156d90f48
jjcm <j@jjcm.org>
2020-07-23 00:09:21 +1000

    added middlewear to set access controls

--- cmd/app.go
+++ cmd/app.go
@@ -14,7 +14,7 @@ func runApp(c *cli.Context) error {
 		http.HandleFunc(path, handler)
 	}
 	for path, handler := range httpd.ProtectedRoutes() {
-		http.HandleFunc(path, middleware.CheckToken(handler))
+		http.HandleFunc(path, middleware.CheckToken(middleware.OpenCors(handler)))
 	}
 	log("Starting web api at port " + sociConfig.AppPort)
 	http.ListenAndServe(":"+sociConfig.AppPort, nil)
--- httpd/middleware/middleware.go
+++ httpd/middleware/middleware.go
@@ -1,6 +1,40 @@
 package middleware
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"soci-backend/httpd/handlers"
+)
 
 // Log this is here so we can share the same logger with the main package
 var Log *logrus.Logger
+
+// ClosedCors only allows reqs from authorized domains
+func ClosedCors(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "https://non.io")
+		w.Header().Set("Access-Control-Max-Age", "604800") // One week
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		if r.Method == "OPTIONS" {
+			handlers.SendResponse(w, "", 200)
+			return
+		}
+		next(w, r)
+	}
+}
+
+// OpenCors allows reqs from all domains
+func OpenCors(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Max-Age", "604800") // One week
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		if r.Method == "OPTIONS" {
+			handlers.SendResponse(w, "", 200)
+			return
+		}
+		next(w, r)
+	}
+}
