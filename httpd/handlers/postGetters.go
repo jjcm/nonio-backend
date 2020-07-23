6667bbbe8ebd7001716e69701f78b23d83d29026
jjcm <j@jjcm.org>
2020-07-24 00:22:24 +1000

    moved all the handlers to a more organized model

--- httpd/handlers/postGetters.go
+++ httpd/handlers/postGetters.go
@@ -8,8 +8,30 @@ import (
 	"strings"
 	"time"
 
+	"soci-backend/httpd/utils"
 	"soci-backend/models"
 )
+
+// GetPostByURL find a specific post in the database and send back a JSON
+// representation of it
+func GetPostByURL(w http.ResponseWriter, r *http.Request) {
+	url := utils.ParseRouteParameter(r.URL.Path, "/posts/")
+	if strings.TrimSpace(url) == "" {
+		sendSystemError(w, errors.New("Please pass a valid URL for us to get you your requested content"))
+		return
+	}
+
+	p := models.Post{}
+	err := p.FindByURL(url)
+	if err != nil {
+		sendNotFound(w, errors.New("we couldn't find a post with the url `"+url+"`"))
+		return
+	}
+
+	// pass a pointer to the post so that it runs through the custom
+	// JSON marshaler
+	SendResponse(w, &p, 200)
+}
 
 // fill the tags for those posts
 func fillPostTags(posts []*models.Post) error {
