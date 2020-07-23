6c6b2a0f7638de456b90f181f47be294f3aa61c0
jjcm <j@jjcm.org>
2020-07-23 21:59:41 +1000

    reversed the check of the token/cors, so that the options call goes through unauthenicated

--- cmd/app.go
+++ cmd/app.go
@@ -14,7 +14,7 @@ func runApp(c *cli.Context) error {
 		http.HandleFunc(path, handler)
 	}
 	for path, handler := range httpd.ProtectedRoutes() {
-		http.HandleFunc(path, middleware.CheckToken(middleware.OpenCors(handler)))
+		http.HandleFunc(path, middleware.OpenCors(middleware.CheckToken(handler)))
 	}
 	log("Starting web api at port " + sociConfig.AppPort)
 	http.ListenAndServe(":"+sociConfig.AppPort, nil)
