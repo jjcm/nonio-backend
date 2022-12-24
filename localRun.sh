4f72da1d7d63cb526ad4c9d9683c76184d0a77cc
g14a <gowtham.m81197@gmail.com>
2022-12-24 18:57:56 +0530

    add ledger entries for every payout and withdrawal from the platform

--- localRun.sh
+++ localRun.sh
@@ -14,8 +14,7 @@ export EMAIL_ACCESS_TOKEN=""
 export EMAIL_REFRESH_TOKEN=""
 export EMAIL_CLIENT_ID=""
 export EMAIL_CLIENT_SECRET=""
-export STRIPE_SECRET_KEY=""
-export STRIPE_PUBLISHABLE_KEY=""
+export STRIPE_KEY=""
 export WEBHOOK_ENDPOINT_SECRET=""
 
 cd ../migrations
