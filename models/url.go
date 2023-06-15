88fff95fa8b7210fd0ecfd9936a2db6da184dd5a
jjcm <j@jjcm.org>
2023-06-14 18:42:36 -0700

    updated url model to check if url is available correctly

--- models/url.go
+++ models/url.go
@@ -2,8 +2,11 @@ package models
 
 // URLIsAvailable - check the database to see if a given URL is available
 func URLIsAvailable(url string) (bool, error) {
+	// remove the leading "/post/url-is-available/"
+	url = url[23:]
+
 	var total int
-	err := DBConn.Get(&total, "SELECT COUNT(*) FROM posts WHERE url = ?", url)
+	err := DBConn.Get(&total, "SELECT COUNT(*) FROM posts where url = ?", url)
 	if err != nil {
 		return false, err
 	}
