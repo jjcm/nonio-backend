9209bfaf53ec9eabeef536235c9c1f5915c536cd
Jacob Miller <j@jjcm.org>
2023-08-02 20:44:36 -0700

    added migration for notifications table

--- migrations/00043_create_notifications_table.sql
+++ migrations/00043_create_notifications_table.sql
@@ -1,12 +1,11 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE `payouts` (
+CREATE TABLE `notifications` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL DEFAULT 0,
-  `amount` float unsigned NOT NULL DEFAULT 10,
-  `payout_date` timestamp NOT NULL DEFAULT "0000-00-00 00:00:00",
+  `comment_id` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `read` boolean NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT NOW(),
-  `tallied` boolean NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
